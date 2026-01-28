50844f027005e8fe7df7a2e438185424db3b5dd3
midoriya <gilakzboyz@gmail.com>
2026-01-28 17:11:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import runpy
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parents[1]  # go from /html → repo root
+ROOT = Path(__file__).resolve().parent
 runpy.run_path(str(ROOT / "streamlit_app_v5.1_fixed_patched_v3.py"), run_name="__main__")
