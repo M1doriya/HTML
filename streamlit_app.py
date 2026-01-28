0ff83c563416f783344f850b1800689b0a255573
midoriya <gilakzboyz@gmail.com>
2026-01-28 16:33:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import runpy
 
 # Streamlit Cloud entrypoint wrapper.
-runpy.run_path('streamlit_app_v5.1_fixed_patched_v2.py', run_name='__main__')
+runpy.run_path('streamlit_app_v5.1_fixed_patched_v3.py', run_name='__main__')
